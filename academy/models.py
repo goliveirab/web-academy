2446ebd80e184dc970486f9913fced70f3ce0aac
Germana Oliveira <germana@vauxoo.com>
2017-04-27 20:31:45 +0000

    [REF] webacademy: define a data model | Teachers

--- academy/models.py
+++ academy/models.py
@@ -2,7 +2,14 @@
 
 from openerp import models, fields, api
 
-# class academy(models.Model):
-#     _name = 'academy.academy'
 
-#     name = fields.Char()
+class academy(models.Model):
+    _name = 'academy.academy'
+
+    name = fields.Char()
+
+
+class Teachers(models.Model):
+    _name = 'academy.teachers'
+
+    name = fields.Char()
