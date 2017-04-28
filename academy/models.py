ecea32724378d5dce47838f43b744e661c6c50b8
Germana Oliveira <germana@vauxoo.com>
2017-04-28 16:49:46 +0000

    [FIX] weabademy: flake8 test

--- academy/models.py
+++ academy/models.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class academy(models.Model):
     _name = 'academy.academy'
 
     name = fields.Char()
+
 
 class Courses(models.Model):
     _name = 'academy.courses'
@@ -15,11 +15,11 @@ class Courses(models.Model):
     name = fields.Char()
     teacher_id = fields.Many2one('academy.teachers', string="Teacher")
 
+
 class Teachers(models.Model):
     _name = 'academy.teachers'
 
     name = fields.Char()
     biography = fields.Html()
     course_ids = fields.One2many('academy.courses', 'teacher_id',
-                                  string="Courses")
-
+                                 string="Courses")
