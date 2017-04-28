c3a84101773d986e6dcadcb6634ea425e1a8a406
Germana Oliveira <germana@vauxoo.com>
2017-04-28 12:27:08 +0000

    [REF] webacademy: add web support

--- academy/controllers.py
+++ academy/controllers.py
@@ -3,16 +3,12 @@ from openerp import http
 
 
 class Academy(http.Controller):
-    @http.route('/academy/academy/', auth='public')
+    @http.route('/academy/academy/', auth='public', website=True)
     def index(self, **kw):
-        return http.request.render('academy.index', {
-                                       'teachers': [
-                                           "Diana Padilla",
-                                           "Jody Carrol",
-                                           "Lester Vaughn"
-                                       ],
-        }) 
-
+        Teachers = http.request.env['academy.teachers']
+        return http.request.render('academy.index',
+               {'teachers': Teachers.search([])}
+        )
 # 
 #     @http.route('/academy/academy/objects/', auth='public')
 #     def list(self, **kw):
